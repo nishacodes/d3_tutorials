15d5b3e9d93b5075600d8d7e865bd2e07709d663
nishacodes <nishabatra27@gmail.com>
2014-02-05 00:29:43 -0500

    move data to external csv file

--- update_pattern/update_pattern.js
+++ update_pattern/update_pattern.js
@@ -1,5 +1,5 @@
 // DEFINE DATASET
-var alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+// var alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
 
 // SET DIMENSIONS OF CONTAINER
 var width = 960,
@@ -11,56 +11,58 @@ var svg = d3.select("body").append("svg")
   .attr("height", height)
   .append("g")
     .attr("transform","translate(32," + (height/2) + ")");
-  
 
-// d3.csv("data.csv", type, function(error, data){ 
+d3.csv("alphabet.csv", function(error, data){ 
 
-// 1) MAKE UPDATE FUNCTION
-function update(data) {
-  
-  // 2) DATA JOIN
-  var text = svg.selectAll("text")
-    .data(data);
+  // 1) MAKE UPDATE FUNCTION
+  function update(data) {
+    
+    // 2) DATA JOIN
+    var text = svg.selectAll("text")
+      .data(data);
 
-  // 3) UPDATE
-  // data and nodes that match up
-  text.attr("class", "update");
+    // 3) UPDATE
+    // data and nodes that match up
+    text.attr("class", "update");
 
-  //ENTER
-  //create new elements as needed
-  text.enter().append("text")
-    .attr("class", "enter")
-    .attr("x", function(d,i) { return i * 32 ;} )
-    .attr("dy", ".35em");
+    //ENTER
+    //create new elements as needed
+    text.enter().append("text")
+      .attr("class", "enter")
+      .attr("x", function(d,i) { return i * 32 ;} )
+      .attr("dy", ".35em");
 
-  // ENTER + UPDATE
-  // Appending to the enter selection expands the update selection to include
-  // entering elements; so, operations on the update selection after appending to
-  // the enter selection will apply to both entering and updating nodes.
-  text.text(function(d) { return d; });
+    // ENTER + UPDATE
+    // Appending to the enter selection expands the update selection to include
+    // entering elements; so, operations on the update selection after appending to
+    // the enter selection will apply to both entering and updating nodes.
+    text.text(function(d) { return d.value; });
 
-  //EXIT
-  //remove old elements as needed
-  text.exit().remove();
-}
-// });
-
-//initial display
-update(alphabet);
-
-//grab random sample of letters
-setInterval(function() {
-  update(shuffle(alphabet)
-      .slice(0, Math.floor(Math.random() * 26))
-      .sort());
-}, 1500);
-
-// Shuffles the input array.
-function shuffle(array) {
-  var m = array.length, t, i;
-  while (m) {
-    i = Math.floor(Math.random() * m--);
-    t = array[m], array[m] = array[i], array[i] = t;
+    //EXIT
+    //remove old elements as needed
+    text.exit().remove();
   }
-  return array;
-}
+
+
+  //initial display
+  update(data);
+
+  //grab random sample of letters
+  setInterval(function() {
+    update(shuffle(data)
+        .slice(0, Math.floor(Math.random() * 26))
+        .sort());
+  }, 1500);
+
+  // Shuffles the input array.
+  function shuffle(array) {
+    var m = array.length, t, i;
+    while (m) {
+      i = Math.floor(Math.random() * m--);
+      t = array[m], array[m] = array[i], array[i] = t;
+    }
+    return array;
+  }
+
+
+});
